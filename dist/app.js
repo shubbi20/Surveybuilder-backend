c1695bd1de1f82d6c756b6b27b9c03cda89d0a77
Shubham negi <negishubham93@gmail.com>
2022-04-24 23:26:57 +0530

    mvc model

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const port = 3009;
 const app = (0, express_1.default)();
+// mongoose.connect(mongo "mongodb+srv://shubham0.4cnas.mongodb.net/myFirstDatabase" --username shubhammongodb)
+//process.env.DATABASE
+//mongodb+srv://userName:Paasword@cluster0.dimbv.mongodb.net/ecomDB    { useNewUrlParser: false }
+mongoose_1.default.connect("mongodb+srv://shubham0.4cnas.mongodb.net/Shubham0", { useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log("Database not connected" + err);
+    }
+    else {
+        console.log("Database connected");
+    }
+});
+// mongoose.connect(
+//   "mongodb+srv://shubhammongodb:Shubhammongodb123@cluster0.dimbv.mongodb.net/ecomDB",{ useNewUrlParser: true },
+//   (err) => {
+//     if (err) {
+//       console.log("Database not connected");
+//     } else {
+//       console.log("Database connected");
+//     }
+//   }
+// );
 app.get("/", (_req, res) => {
     res.end("Hello World!");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,36 @@
 import express from "express";
+import mongoose, { ConnectOptions } from "mongoose";
+import bodyParser from "body-parser";
 
 const port = 3009;
 const app = express();
+
+// mongoose.connect(mongo "mongodb+srv://shubham0.4cnas.mongodb.net/myFirstDatabase" --username shubhammongodb)
+//process.env.DATABASE
+//mongodb+srv://userName:Paasword@cluster0.dimbv.mongodb.net/ecomDB    { useNewUrlParser: false }
+
+mongoose.connect(
+  "mongodb+srv://shubham0.4cnas.mongodb.net/Shubham0",
+  { useNewUrlParser: true } as ConnectOptions,
+  (err) => {
+    if (err) {
+      console.log("Database not connected" + err);
+    } else {
+      console.log("Database connected");
+    }
+  }
+);
+
+// mongoose.connect(
+//   "mongodb+srv://shubhammongodb:Shubhammongodb123@cluster0.dimbv.mongodb.net/ecomDB",{ useNewUrlParser: true },
+//   (err) => {
+//     if (err) {
+//       console.log("Database not connected");
+//     } else {
+//       console.log("Database connected");
+//     }
+//   }
+// );
 
 app.get("/", (_req, res) => {
   res.end("Hello World!");
